3b616f37441603decf7441a3b593d3fb9faaca7c
PauloRolim <pauloerb@hotmail.com>
2021-02-19 16:44:02 -0300

    POG

--- busca_binaria.cpp
+++ busca_binaria.cpp
@@ -46,20 +46,87 @@ bool buscaBinariaPlus (int *vetor, int n_elementos, int chave){
     return false;
 }
 
+int buscaIntersec (int *vetor1, int *vetor2){
 
-int main(){
+    int tamanho = 0;
 
-    int vetorEntrada[6] = {2,3,5,6,8,9};
+    int len_L1 = sizeof(vetor1) / sizeof(vetor1[0]);
+    int len_L2 = sizeof(vetor2) / sizeof(vetor2[0]);
 
-    if (buscaBinariaPlus(vetorEntrada, 6, 6) == true)
+    for (int ii = 0; ii < len_L1; ii++)
     {
-        std::cout << "Achei mizera!" << std::endl;
-    } else
+        for (int jj = 0; jj < len_L2; jj++)
+        {
+            if (vetor2[jj] == vetor1[ii])
+            {
+                tamanho++;
+
+            }
+            
+        }
+        
+    }
+
+    int vetor_saida[tamanho] = {};
+
+    for (int ii = 0; ii < len_L1; ii++)
     {
-        std::cout << "Não achei..." << std::endl;
+        for (int jj = 0; jj < len_L2; jj++)
+        {
+            if (vetor2[jj] == vetor1[ii])
+            {
+                vetor_saida[ii] = vetor2[jj];
+
+            }
+            
+        }
+        
+    }
+
+    return vetor_saida[tamanho];
+}
+
+int main(int argc, char *argv[]){
+
+    int L1[4] = {2,5,5,5};
+    int L2[6] = {2,2,3,5,5,7};
+
+    int tamanho = 0;
+    
+    int len_L1 = sizeof(L1) / sizeof(L1[0]);
+    int len_L2 = sizeof(L2) / sizeof(L2[0]);
+
+    for (int ii = 0; ii < len_L1; ii++)
+    {
+        for (int jj = 0; jj < len_L2; jj++)
+        {
+            if (L2[jj] == L1[ii])
+            {
+                tamanho++;
+
+            }
+            
+        }
+        
     }
     
-    
+    int L3[tamanho] = {};
+
+    for (int ii = 0; ii < len_L1; ii++)
+    {
+        for (int jj = 0; jj < len_L2; jj++)
+        {
+            if (L2[jj] == L1[ii])
+            {
+                L3[ii] = L2[jj];
+
+            }
+            
+        }
+        
+    }
+
+    std::cout << "L3 = {" << L3[0] << ", " << L3[1] << ", " << L3[2] << "}" << std::endl;*/
 
     return 0;
 }
